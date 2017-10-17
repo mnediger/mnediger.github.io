ec7b4bbe087f962f772e712ba763b50d5e2b45af
mnediger <m.nediger@mail.utoronto.ca>
2017-10-17 15:42:48 -0400

    Moved validation script to contact

--- js/jquery.js
+++ js/jquery.js
@@ -33,45 +33,4 @@ $(document).ready(function() {
 
     });
     
-    // Validates the contact form and submits it using Formspree.
-    $.validator.setDefaults({
-        highlight: function(element) {
-            $(element).closest('.form-group').addClass('has-error');
-        },
-        unhighlight: function(element) {
-            $(element).closest('.form-group').removeClass('has-error');
-        },
-        errorElement: 'span',
-        errorClass: 'help-block',
-        errorPlacement: function(error, element) {
-            if(element.parent('.input-group').length) {
-                error.insertAfter(element.parent());
-            } else {
-                error.insertAfter(element);
-            }
-        }
-    });
-
-    $("#contact-form").validate({
-      submitHandler: function(form) {
-        $.ajax({
-          url: "https://formspree.io/m.nediger@gmail.com", 
-          method: "POST",
-          data: {
-            name: $(form).find("input[name='name']").val(),
-            _replyto: $(form).find("input[name='_replyto']").val(),
-            message: $(form).find("textarea[name='message']").val()
-          },
-          dataType: "json",
-          success: function() {
-            $("#submit-success").fadeIn();
-            $("#contact-form").fadeOut();
-          },
-          error: function() {
-            $("#submit-errors").fadeIn();        
-          }
-        });
-      }
-    });
-    
 })
